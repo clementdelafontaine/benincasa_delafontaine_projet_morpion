fdd7aace02bf8cd677ba84afc29fb89c16e0cbb3
clementdelafontaine <clement-delafontaine@etu-umontpellier.fr>
2021-11-08 17:33:29 +0100

    update

---
+++ Serveur.java
@@ -0,0 +1,73 @@
+import java.net.*;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.PrintWriter;
+
+public class Serveur implements Runnable {
+    private BufferedReader in;
+    private PrintWriter out;
+    private ServerSocket sockserv = null;
+    private Socket sockcli = null;
+    private String message;
+    private String clientsEnJeu[];
+    private String clientsEnAttente[];
+
+    public Serveur(){
+        try {
+            sockserv = new ServerSocket(1234); // Création du socket serveur
+        } catch (IOException e) {}
+    }
+
+    public boolean connexion() {
+        try {
+            sockcli = sockserv.accept(); // Attente  requête client
+            return true;
+        } catch (Exception e) { return false; }
+    }
+
+    public String getSockcli(){
+        return sockcli.toString();
+    }
+
+    public void run() {
+        try {
+
+            // Création de flux de communication
+            in = new BufferedReader(new InputStreamReader(sockcli.getInputStream()));
+            out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(sockcli.getOutputStream())), true);
+            
+            // Création
+
+            while(true){
+                message = in.readLine();
+                System.out.println("message reçu : "+ message);
+                out.println(message);
+
+                if (message.equals("103")) {
+                    // Vérifier s'il y a des joueurs dans la liste d'attente
+                    if (clientsEnAttente.length % 2 != 0) {
+                        // Lancer le thread avec la dernière personne de la liste
+
+                    }
+                    
+                }
+
+                out.flush();
+
+                if(mess.substring(0, 4).equals("stop")){
+                    break;
+                }
+            }
+
+            sockcli.close(); // Fermeture port de communication
+        } catch (Exception e) {
+            System.out.println("Déconnexion du client : "+e);
+        }
+    }
+}
