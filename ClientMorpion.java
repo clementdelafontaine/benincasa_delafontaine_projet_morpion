f2245590bea1da70f96b2c06f6266a66efbb3239
benincasaj <benincasaj@K114-1-16.dep-info.iutmontp.univ-montp2.fr>
2021-12-13 16:17:10 +0100

    update client

--- ClientMorpion.java
+++ ClientMorpion.java
@@ -10,39 +10,48 @@ import java.util.Scanner;
 public class ClientMorpion {
     public ClientMorpion (String adresse, int port) {
         try {
+            //création socket client
             Socket socket = new Socket(adresse, port);
 
+            //création des buffers de communication
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
 
-            Scanner sc = new Scanner(System.in);;
+            //création du scanner pour l'input client
+            Scanner sc = new Scanner(System.in);
+
+            //compteur bidon (utile pour l'affichage du plateau de jeu)
+            int n = 0;
                 
-            // Demande de connexion
+            //demande de connexion
             out.println("103");
             
-            // lecture message d'entree
+            //variable de stockage pour les codes reçus par le serveur
             String message = "";
-            String messageEnvoi = "";
 
-            //Tant que je ne reçoit pas de code, attente.
+            //tant que je ne reçoit pas de code, attente.
             while((message = in.readLine())==null) ;
                     
-            // 104 : partie terminée
+            //(104 : partie terminée) tant que la partie n'est pas terminée
             while (!message.equals("104"))
             {
                 try
                 {
+                    //évaluation des codes reçus par le serveur
                     switch (message) {
-                            case "100":
+                        case "100":
                             System.out.println("En attente d'un opposant ... \n");
-                            break;
+                        break;
                         case "101":
-                            // Print tableau de jeu
+                            //nettoyage console
                             System.out.print("\033[H\033[2J");
-                            while(in.ready()) {
-                                System.out.println(in.readLine());
-                            }
                             System.out.println("C'est au tour de l'adversaire \n");
+                            //lecture des string représentant le tableau 
+                            while (n<9) {
+                                System.out.println(in.readLine());
+                                n++;
+                            }
+                            n=0;
                         break;
                         // End of game
                         case "102":
@@ -53,14 +62,17 @@ public class ClientMorpion {
                             System.out.println("Lancement de la partie \n");
                         break;
                         case "202":
+                            //nettoyage console
                             System.out.print("\033[H\033[2J");  
-                            System.out.println("A votre tour, saisir une case : \"ligne colonne\"");
-                            while(in.ready()) {
+                            System.out.println("A votre tour, saisir une ligne puis une colonne");
+                            //lecture des lignes représentant le tableau
+                            while (n<9) {
                                 System.out.println(in.readLine());
+                                n++;
                             }
+                            n=0;
                             String ligne = sc.nextLine();
                             String colonne = sc.nextLine();
-                            System.out.println("L"+ligne+":C"+colonne);
                             out.println(ligne);
                             out.println(colonne);
                         break;
@@ -69,7 +81,7 @@ public class ClientMorpion {
                             System.out.println(message);
                         break;
                     }
-                    while((message = in.readLine())==null);
+                    while(message != "104" && (message = in.readLine())==null){System.out.println("yes");}
 
                 }
                 catch(IOException i)
@@ -77,22 +89,12 @@ public class ClientMorpion {
                     System.out.println(i);
                 }
             }
-            
-            /*
-            // close the connection
-            try
-            {
-                in.close();
-                out.close();
-                socket.close();
-            }
-            catch(IOException i)
-            {
-                System.out.println(i);
-            }
 
+            in.close();
+            out.close();
             socket.close();
-            */
+
+            
         } catch(IOException e){
             System.out.println("Erreur : " + e);
         }
